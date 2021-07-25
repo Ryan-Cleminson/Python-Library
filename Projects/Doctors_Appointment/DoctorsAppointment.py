8fd49bf6d0a9e735f6f6f51a76001d27ae693324
Ryan Cleminson <rdcleminson@gmail.com>
2021-07-25 19:00:07 +1000

    Push

---
+++ Projects/Doctors_Appointment/DoctorsAppointment.py
@@ -0,0 +1,28 @@
+#############################################################################
+# Author: Ryan Cleminson (Ryan.Cleminson@student.uts.edu.au)
+# Date: 2/07/2020
+# Purpose: 
+#############################################################################
+
+import numpy as np
+import pandas as pd
+
+
+class Records:
+    pass
+
+PDetails = Records()
+
+PDetails.PName[]
+
+
+
+def __init__(self):
+    Arraysize = 100
+    AppTime = 7
+    AppDay = 8
+
+
+
+
+
